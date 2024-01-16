f5fc7cb0bdce67ab361d252bf9c3448f86917f51
Sarah-Rochette <sarah.rochette@insa-strasbourg.fr>
2024-01-16 18:48:14 +0100

    etat initial

--- src/main/java/fr/insa/rochette/cours/m3/projets/likes/model/view/LoginView.java
+++ src/main/java/fr/insa/rochette/cours/m3/projets/likes/model/view/LoginView.java
@@ -16,6 +16,8 @@ import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import fr.insa.rochette.cours.m3.projets.likes.model.Utilisateur;
+import java.util.ArrayList;
 
 @Route("login") 
 
@@ -36,8 +38,13 @@ public class LoginView extends VerticalLayout implements BeforeEnterObserver {
 		setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
 
 		this.login.setAction("login");
-                this.login
+                 this.login.addLoginListener(event-> {
+                     String mp = event.getPassword();
+                     String login= event.getUsername();
+               // ArrayList<Utilisateur> web = tousLesUtilisateurs(ConnectinSGBD connSGBD);
                 
+                     
+                 });
 
 		add(new H1("Gestion de Production"), login);
 	}
